ec967f0c7feaac2168ca4c3a67df328065eda451
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2023-09-02 11:52:58 +0200

    number helpers

---
+++ helpers/number.ts
@@ -0,0 +1,11 @@
+export const safeDivide = (dividend: number, divisor: number, returnZero?: true) => {
+    if (divisor === 0) {
+        return returnZero ? 0 : null
+    }
+    return dividend / divisor
+}
+
+export const roundNumber = (number: number, decimalPlaces: number) => {
+  const multiplier = Math.pow(10, decimalPlaces);
+  return Math.round(number * multiplier) / multiplier;
+};
