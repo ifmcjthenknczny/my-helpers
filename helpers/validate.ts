74c9e22f7e4b8c1a72d2cc7d704fb2d7098bf5dd
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2023-09-25 18:35:44 +0200

    validateSchema type fix

--- helpers/validate.ts
+++ helpers/validate.ts
@@ -5,7 +5,7 @@ export const hasNumber = (text: string) => /\d/.test(text);
 export const validateEmail = (mail: string) =>
   !!mail.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
 
-export const validateSchema = <T>(data: T, schema: Schema<T>): T => {
+export const validateSchema = <T>(data: Partial<T>, schema: Schema<T>): T => {
   const { error, value } = schema.validate(data);
   if (error) {
     throw new Error(`Validation error: ${error.message}`);
